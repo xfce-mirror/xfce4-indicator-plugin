a7ae93ad809b386634f84e11c6ef1bea0eb3b6ca
Andrzej <ndrwrdck@gmail.com>
2013-04-05 01:00:00 +0100

    button: Compilation fix.
    There is no 'orientation' field in IndicatorButton. The label orientation
    is set in the following _update_layout() call.

--- panel-plugin/indicator-button.c
+++ panel-plugin/indicator-button.c
@@ -307,8 +307,6 @@ xfce_indicator_button_set_label (XfceIndicatorButton *button,
 
       button->label = GTK_WIDGET (label);
       g_object_ref (G_OBJECT (button->label));
-      gtk_label_set_angle (GTK_LABEL (button->label),
-                           (button->orientation == GTK_ORIENTATION_VERTICAL) ? -90 : 0);
       gtk_box_pack_end (GTK_BOX (button->box), button->label, TRUE, FALSE, 1);
       g_signal_connect(G_OBJECT(button->label), "notify::label", G_CALLBACK(xfce_indicator_button_label_changed), button);
     }
